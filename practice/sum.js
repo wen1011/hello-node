754e6922008f7dd818b81f3a26104a0acfaad36c
wen1011 <88265377+wen1011@users.noreply.github.com>
2021-09-19 16:42:53 +0800

    完成測試

---
+++ practice/sum.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+function sum(param) {
+  var i;
+  var sum = 0;
+  for (i = 1; i <= param; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+console.log(sum(3)); // 6
+console.log(sum(6)); // 21
+console.log(sum(10)); // 55
